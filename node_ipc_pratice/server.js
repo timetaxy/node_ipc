c3eee2a00cc0553a8017054d0289ac7025625620
= <fantasyz7@naver.com>
2021-08-24 17:15:01 +0900

    node ipc comm

---
+++ node_ipc_pratice/server.js
@@ -0,0 +1,35 @@
+const { RootIPC } = require('node-ipc');
+const ipc = RootIPC;
+// import ipc from '../../../node-ipc.js';
+
+/***************************************\
+ *
+ * You should start both hello and world
+ * then you will see them communicating.
+ *
+ * *************************************/
+
+ipc.config.id = 'world';
+ipc.config.retry = 1500;
+
+ipc.serve(
+    function () {
+        ipc.server.on(
+            'app.message',
+            function (data, socket) {
+                ipc.server.emit(
+                    socket,
+                    'app.message',
+                    {
+                        id: ipc.config.id,
+                        message: data.message + ' world!'
+                    }
+                );
+            }
+        );
+    }
+);
+
+
+
+ipc.server.start();
