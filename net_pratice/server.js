a8a23c67cdee3015101e028b15f48d6577c04080
= <fantasyz7@naver.com>
2021-08-29 15:16:05 +0900

    redis pubsub test

--- net_pratice/server.js
+++ net_pratice/server.js
@@ -1,79 +1,37 @@
 const net = require('net');
 const PORT = 8124;
-const HOST = '127.0.0.1';
-// let clientConn = false;
-// let clientReady = false;
+const HOST = '::1';
 let dataQ = [];
 let socket;
 const server = net.createServer(s => {
-    // tempSocket = socket;
     socket = s;
     s.setEncoding('utf-8');
     s.on('connect', x => { console.log(`server, conn recv : ${x}`); });
     console.log(`socket._server._connections : ${s._server._connections}`);
-    // if (socket._server._connections) clientConn = true;
     s.on('data', d => {
         console.log(`this server, receive from client data:${d}`);
         //include self, broad cast
-        // if (d.trim().toLowerCase() === 'ready') clientReady = true;
     });
-    // while (true) {
-    //     console.log(`before send queue : ${JSON.stringify(dataQ)}`);
-    //     if (clientConn && dataQ.length > 0) {
-    //         // if (clientConn && clientReady && dataQ.length > 0) {
-    //         socket.write(JSON.stringify(dataQ.shift()));
-    //         console.log(`rest of this queue : ${JSON.stringify(dataQ)}`);
-    //     }
-    // }
     s.on('error', e => {
         console.log(`err occurr e:${e}`);
-        // clientConn = false, 
-        // clientReady = false; 
         return s.end();
     });
-}).listen(PORT, '::1', () => {
-    // }).listen(PORT, HOST, () => {
+}).listen(PORT, HOST, () => {
     console.log('server bound');
 });
 server.on('error', (err) => {
     // throw err;
-    // server.removeAllListeners();
 });
 
-// (() => {
 let i = 0;
 setInterval(() => { console.log(`now data emitting`); dataQ.push({ num: i++, data: 'new data' }); console.log(`emitting rest data : ${JSON.stringify(dataQ)}`); }, 1000);
-// })();
 
 setInterval(() => {
     console.log(`server._connections:${server._connections}`);
-    // if (server.connections && dataQ.length > 0) {
-    // if (clientConn && clientReady && dataQ.length > 0) {
     if (server._connections) {
-        // console.log(`tempSocket._server.connections:${tempSocket._server._connections}`);
         socket.write(JSON.stringify(dataQ.shift()));
         console.log(`rest of this queue : ${JSON.stringify(dataQ)}`);
     }
     // }
 }, 1000);
-// .listen(this._pipe);
-// server.unref();
-// const server = net.createServer((c) => {
-//     // 'connection' listener.
-//     console.log('client connected');
-//     c.on('end', () => {
-//         console.log('client disconnected');
-//     });
-//     c.on('data',()=>{
-
-//     })
-//     // c.write('hello\r\n');
-//     // c.pipe(c);
-// });
-
-// let i = 0;
-// setInterval(() => { c.write(`here is server ${i++}\r\n`); }, 1000);
-// setInterval(() => { server.emit(`here is server ${i++}\r\n`); console.log(`data emitting`); }, 1000);
-
-// if (server) server.once('data', () => { console.log(`data send`); });
 
---
+++ redis_sample/redis_sample.js
@@ -0,0 +1,23 @@
+const { createClient } = require('redis');
+const client = createClient(6370);
+client.on('error', () => { console.log('redis error'); });
+client.on('ready', () => { console.log('redis ready'); });
+client.on('connect', () => { console.log('redis connected'); });
+(async () => {
+    // client.on('error', (err) => { console.log(`redis client err: ${err}`); });
+    await client.connect();
+    // await client.set('k', 'v');
+    // console.log(await client.get('k'));
+    const sub = client.duplicate();
+    await sub.connect();
+    sub.subscribe('noti2', (msg) => {
+        console.log(`this sub2, msg:${msg}`);
+    });
+
+    // publish
+    // client.publish('noti', 'test');
+
+    // disconn
+    // await sub.unsubscribe('noti');
+    // await sub.pUnsubscribe('noti');
+})();
